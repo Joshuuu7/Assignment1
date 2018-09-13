2e10c2bc306fed5934ad8b26469020fc50212366
Joshua Flores <joshuaaaronflores@gmail.com>
2018-09-13 09:47:08 -0500

    Added a couple files and fixed the errors in DetailViewController.

--- Assignment1/DetailViewController.swift
+++ Assignment1/DetailViewController.swift
@@ -11,12 +11,43 @@ import WebKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var airDateLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var summaryWebView: WKWebView!
+    
     func configureView() {
         // Update the user interface for the detail item.
-        let detail = detailItem
+        if let detail = detailItem {
+            if let imageView = self.imageView {
+                downloader!.downloadImage(urlString: detail.image.medium) {
+                    (image: UIImage?) in
+                    imageView.image = image
+                }
+            }
+            if let webView = summaryWebView {
+                let htmlString = "<html><head><meta name=\"viewport\" content=\"initial-scale=1.0\" /><style>body { font-family: -apple-system;font-size:12pt }</style></head><body>" + detail.summary + "</body></html>"
+                webView.loadHTMLString(htmlString, baseURL: nil)
+            }
+            if let label = titleLabel {
+                label.text = detail.name
+            }
+            if let label = airDateLabel {
+                label.text = detail.airdate
+            }
+        }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     var detailItem: Episode? {
         didSet {
@@ -25,4 +56,5 @@ class DetailViewController: UIViewController {
         }
     }
     
+    var downloader: Downloader?
 }
--- Assignment1/Episode.swift
+++ Assignment1/Episode.swift
@@ -8,12 +8,35 @@
 
 import Foundation
 
+struct EpisodeImage: Decodable {
+    let medium: String
+    let original: String
+}
+
 struct Episode: Decodable {
     let name: String
-    let artist: Int
+    let season: Int
     let number: Int
     let airdate: String
+    let image: EpisodeImage
     let summary: String
 }
+
+struct EpisodeData: Decodable {
+    let episodes: [Episode]
+}
+
+struct ShowData: Decodable {
+    let name: String
+    let summary: String
+    let embedded: EpisodeData
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case summary
+        case embedded = "_embedded"
+    }
+}
+
 
 
